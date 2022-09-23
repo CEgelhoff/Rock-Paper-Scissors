aecdae9b0f716092188747ebf1adbffde1b0b069
CEgelhoff <christoph.egelhoff@gmail.com>
2022-09-23 12:15:57 -0700

    Each round is now returned in round function

--- rps.js
+++ rps.js
@@ -1,6 +1,6 @@
-function getComputerChoice(cChoice){
+function getComputerChoice(){
     cChoice = Math.floor(Math.random() * 3);
-    console.log(cChoice);
+    
     if (cChoice === 0){
         console.log("rock")
         cChoice = 'rock'
@@ -24,7 +24,7 @@ function getPlayerChoice(pChoice){
     return pChoice
 }
 
-function oneRound(c, p){
+/* function oneRound(c, p){
     c = getComputerChoice();
     p = getPlayerChoice();
     p = p.toLowerCase();
@@ -40,9 +40,26 @@ function oneRound(c, p){
     else if (p === 'paper' && c === 'scissors') console.log('You chose ' + p + ' and the computer chose ' + c + '. You Lose!')
     else if (p === 'scissors' && c === 'paper') console.log('You chose ' + p + ' and the computer chose ' + c + '. You Win!')
     else {console.log('Please input a valid option. (ex):Rock Paper or Scissors')}
-}
+} */
 
-oneRound();
+
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) return 'Tie!'
+    else if (playerSelection === 'paper' && computerSelection === 'rock') return 'You Win!'
+    else if (playerSelection === 'rock' && computerSelection === 'paper') return 'You Lose!'
+    else if (playerSelection === 'scissors' && computerSelection === 'rock') return 'You Lose!'
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') return 'You Win!'
+    else if (playerSelection === 'paper' && computerSelection === 'scissors') return 'You Lose!'
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') return 'You Win!'
+    else {console.log('Please input a valid option. (ex):Rock Paper or Scissors')}
+  }
+   
+  const playerSelection = 'paper';
+  const computerSelection = getComputerChoice();
+
+  console.log(playRound(playerSelection, computerSelection));
 
 
 
